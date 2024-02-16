602f7a935226d8047b8db046ee608beac02c9fed
Heorhii Mykhailenko <georgy_vasilievich@ukr.net>
2024-02-16 17:30:30 +0200

    Simplify Lection 3 Task 1

--- Lection_3/Task_1/mainwindow.cpp
+++ Lection_3/Task_1/mainwindow.cpp
@@ -51,20 +51,10 @@ void MainWindow::Crypt()
             if((input[character] < 'A' || input[character] > 'Z') && (input[character] < 'a' || input[character] > 'z'))
                 continue;
 
-            if (decrypt)
-            {
-                if (input[character].isUpper())
-                    input[character] = QChar((26 + int(input[character].toLatin1() - ASCII_UPPERCASE - key) % 26) % 26 + ASCII_UPPERCASE);
-                else
-                    input[character] = QChar((26 + int(input[character].toLatin1() - ASCII_LOWERCASE - key) % 26) % 26 + ASCII_LOWERCASE);
-            }
+            if (input[character].isUpper())
+                input[character] = QChar((26 + int(input[character].toLatin1() - ASCII_UPPERCASE - (key * (decrypt ? 1 : -1))) % 26) % 26 + ASCII_UPPERCASE);
             else
-            {
-                if (input[character].isUpper())
-                    input[character] = QChar(int(input[character].toLatin1() - ASCII_UPPERCASE + key) % 26 + ASCII_UPPERCASE);
-                else
-                    input[character] = QChar(int(input[character].toLatin1() - ASCII_LOWERCASE + key) % 26 + ASCII_LOWERCASE);
-            }
+                input[character] = QChar((26 + int(input[character].toLatin1() - ASCII_LOWERCASE - (key * (decrypt ? 1 : -1))) % 26) % 26 + ASCII_LOWERCASE);
     }
 
     ui->editOutput->setText(input);
